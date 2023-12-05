02c74269b8ff0235da1b66836db2d4b24d86c505
carloscca10 <carlos.ccag10@gmail.com>
2023-12-05 16:28:44 +0100

    deshacer cambios

--- brute_force/nbody.c
+++ brute_force/nbody.c
@@ -5,7 +5,7 @@
 #include <stddef.h>
 #include "parameters.h"
 #include "nbody_bruteforce.h"
-#include "nbody_barneshut.h"
+//#include "nbody_barneshut.h"
 #include "reader.h"
 #include <sys/time.h>
 
