1831efcc8f1a658e024bbabe8659fdc5d0baf8d2
Carlos Collado Capell <ccollado@izar1.izar.cluster>
2023-12-08 13:56:40 +0100

    gitlab cuda

---
+++ cuda/nbody_bruteforce.h
@@ -0,0 +1,16 @@
+#ifndef NBODYBRUTEFORCE_H_
+#define NBODYBRUTEFORCE_H_
+
+#include <stdio.h> 
+#include "parameters.h"
+
+#define max(a,b) ((a) > (b) ? (a) : (b))
+#define min(a,b) ((a) < (b) ? (a) : (b))
+
+
+void nbodybruteforce (particle_t * array, int nbr_particles, int nbr_iterations) ;
+void compute_brute_force(particle_t * p1, particle_t * array, int nbr_particles, double step) ;
+void update_positions(particle_t * array, int nbr_particles, double step) ;
+
+
+#endif /*NBODYBRUTEFORCE_H_*/
