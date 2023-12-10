f290cf47030ce14376239515febee3c532be03c5
carloscca10 <carlos.ccag10@gmail.com>
2023-12-10 21:48:08 +0100

    new cuda

---
+++ cuda_code/script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --job-name=nbody_simulation
+#SBATCH -N 1            # Number of nodes
+#SBATCH -n 1            # Number of cores
+#SBATCH --account=math-454
+#SBATCH --reservation=Course-math-454-final
+#SBATCH --time=01:00:00 # Time limit hrs:min:sec
+#SBATCH --output=nbody_%j.out # Standard output and error log
+#SBATCH --error=nbody_%j.err
+
+module purge
+module load gcc cuda
+
+srun nvprof ./nbody-code
+
