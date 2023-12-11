b9323ab6bf23285cca48cbaaacfe5ac9d212cb8e
carloscca10 <carlos.ccag10@gmail.com>
2023-12-11 17:22:02 +0100

    na

--- cuda_code/script.sh
+++ cuda_code/script.sh
@@ -3,6 +3,9 @@
 #SBATCH --job-name=nbody_simulation
 #SBATCH -N 1            # Number of nodes
 #SBATCH -n 1            # Number of cores
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --qos=gpu_free
 #SBATCH --account=math-454
 #SBATCH --reservation=Course-math-454-final
 #SBATCH --time=01:00:00 # Time limit hrs:min:sec
