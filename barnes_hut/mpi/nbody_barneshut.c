69165896dc7c4ea94bd59f4372519ee3488d6b47
carloscca10 <carlos.ccag10@gmail.com>
2023-12-17 00:27:20 +0100

    na

--- barnes_hut/mpi/nbody_barneshut.c
+++ barnes_hut/mpi/nbody_barneshut.c
@@ -47,7 +47,7 @@ void nbodybarneshut (particle_t * array, int nbr_particles, int nbr_iterations, 
 		clean_tree(root2);
 
 		int i;
-		for(i=0, i<nbr_particles, i++) {
+		for(i=0; i<nbr_particles; i++) {
 			printf("Particle %i | %i -> (%f, %f, %f) \n", array[i].id, array[i].mpi_id, array[i].fx, array[i].fy, array[i].fz);
 		}
 	}
