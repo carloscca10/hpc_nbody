483ea9f60770e08050bb2d8dd23e6f74d3b813b5
carloscca10 <carlos.ccag10@gmail.com>
2023-12-16 12:15:48 +0100

    v1 mpi

--- barnes_hut/mpi/nbody_barneshut.c
+++ barnes_hut/mpi/nbody_barneshut.c
@@ -37,7 +37,7 @@ void nbodybarneshut (particle_t * array, int nbr_particles, int nbr_iterations, 
 		compute_force_in_node(root1, root1, prank, psize);
 		compute_bh_force(root1, prank, psize);
 
-		gather_force_vector(array, nbr_particles, &forces);
+		gather_force_vector(array, nbr_particles, forces);
 		MPI_Allreduce(MPI_IN_PLACE, &forces, nbr_particles*3, MPI_FLOAT, MPI_SUM, MPI_COMM_WORLD);
 		broadcast_force_vector(array, nbr_particles, forces);
 
