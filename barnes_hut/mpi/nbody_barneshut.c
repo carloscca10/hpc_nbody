33a4b1fb07717ce88e2ce23d7d3a23d3f91978de
carloscca10 <carlos.ccag10@gmail.com>
2023-12-16 12:14:18 +0100

    v1 mpi

--- barnes_hut/mpi/nbody_barneshut.c
+++ barnes_hut/mpi/nbody_barneshut.c
@@ -37,9 +37,9 @@ void nbodybarneshut (particle_t * array, int nbr_particles, int nbr_iterations, 
 		compute_force_in_node(root1, root1, prank, psize);
 		compute_bh_force(root1, prank, psize);
 
-		gather_force_vector(&array, nbr_particles, &forces);
+		gather_force_vector(array, nbr_particles, &forces);
 		MPI_Allreduce(MPI_IN_PLACE, &forces, nbr_particles*3, MPI_FLOAT, MPI_SUM, MPI_COMM_WORLD);
-		broadcast_force_vector(&array, nbr_particles, forces);
+		broadcast_force_vector(array, nbr_particles, forces);
 
 		move_all_particles(root2, root1, step);
 
@@ -508,9 +508,9 @@ OTHER MPI FUNCTIONS
 void gather_force_vector(particle_t *array, int nbr_particles, double *forces) {
     int i;
     for (i = 0; i < nbr_particles; i++) {
-        forces[3*i] = array[i].fx;    // x-component of force for particle i
-        forces[3*i + 1] = array[i].fy; // y-component of force for particle i
-        forces[3*i + 2] = array[i].fz; // z-component of force for particle i
+        forces[3*i] = &array[i].fx;    // x-component of force for particle i
+        forces[3*i + 1] = &array[i].fy; // y-component of force for particle i
+        forces[3*i + 2] = &array[i].fz; // z-component of force for particle i
     }
 }
 
@@ -518,8 +518,8 @@ void gather_force_vector(particle_t *array, int nbr_particles, double *forces) {
 void broadcast_force_vector(particle_t *array, int nbr_particles, double forces) {
 	int i;
 	for (i = 0; i < nbr_particles; i++) {
-		array[i].fx = forces[3*i];    // x-component of force for particle i
-		array[i].fy = forces[3*i + 1]; // y-component of force for particle i
-		array[i].fz = forces[3*i + 2]; // z-component of force for particle i
+		&array[i].fx = forces[3*i];    // x-component of force for particle i
+		&array[i].fy = forces[3*i + 1]; // y-component of force for particle i
+		&array[i].fz = forces[3*i + 2]; // z-component of force for particle i
 	}
 }
