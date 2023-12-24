10e66c3348de76e37c4122997afbd52ba2a0d7a9
carloscca10 <carlos.ccag10@gmail.com>
2023-12-24 10:11:29 +0100

    a

--- cuda_code/script.sh
+++ cuda_code/script.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --reservation=Course-math-454-final
-#SBATCH --account=math-454
-#SBATCH --time=01:00:00 # Time limit hrs:min:sec
-#SBATCH -N 1            # Number of nodes
-#SBATCH -n 1            # Number of cores
+#SBATCH --nodes=1
+#SBATCH --time=01:00:00
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
+#SBATCH --account=math-454
+#SBATCH --reservation=Course-math-454-final
 
 module purge
 module load gcc cuda
 module list
 
-srun nbody-code $1
+#nvprof is used to profile our code
+srun nvprof nbody-code $1
