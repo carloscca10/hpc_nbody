570d9019829391bc13f54b2ad27a2f9153667ddb
carloscca10 <carlos.ccag10@gmail.com>
2023-12-24 18:51:15 +0100

    mpi wo prints

--- barnes_hut/no_print_mpi/script.sh
+++ barnes_hut/no_print_mpi/script.sh
@@ -8,4 +8,12 @@ module purge
 module load intel intel-oneapi-mpi
 
 ulimit -l 127590
-srun nbody-code $1
+
+# Define the base directory for the subset files
+BASE_DIR="../../examples/strong_scaling"
+
+# Loop over the subset files from 10000 to 140000
+for i in {10000..140000..10000}
+do
+   srun nbody-code "${BASE_DIR}/subset_${i}.txt"
+done
