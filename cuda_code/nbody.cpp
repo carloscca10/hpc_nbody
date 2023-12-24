3aa37c0560e9f92b31ea07afcc7068cead87b021
carloscca10 <carlos.ccag10@gmail.com>
2023-12-24 12:13:48 +0100

    a

--- cuda_code/nbody.cpp
+++ cuda_code/nbody.cpp
@@ -63,6 +63,7 @@ int main ( int argc, char **argv ) {
 	else    
 	{ 
 		printf("Read data from file \n");
+		printf("argc = %d \n",argc);
 		nbr_particles = get_nbr_particles(argv[1]);
 		array = read_test_case(argv[1]);
 		printf("Number of particles : %d\n",nbr_particles);
