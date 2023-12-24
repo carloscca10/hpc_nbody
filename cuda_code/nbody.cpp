f448031b3cfdc28d7532f6d5bb788c0638797e24
carloscca10 <carlos.ccag10@gmail.com>
2023-12-24 13:15:40 +0100

    a

--- cuda_code/nbody.cpp
+++ cuda_code/nbody.cpp
@@ -73,7 +73,7 @@ int main ( int argc, char **argv ) {
 	t1 = second();
 	nbodybruteforce(array, nbr_particles, nbr_iterations);
 	t2 = second();
-	printf("N-Body brute force for %d particles : %f [s] \n",nbr_iterations, (t2-t1));
+	printf("N-Body brute force for %d particles and %d iterations : %f [s] \n",nbr_particles, nbr_iterations, (t2-t1));
 	free(array);
 
 	printf("Simulation finished \n");
