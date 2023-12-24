5271186fda278433424b6258bf6800c7506fcbff
carloscca10 <carlos.ccag10@gmail.com>
2023-12-24 18:46:56 +0100

    mpi wo prints

--- barnes_hut/no_print_mpi/nbody_barneshut.c
+++ barnes_hut/no_print_mpi/nbody_barneshut.c
@@ -64,7 +64,7 @@ void nbodybarneshut (particle_t * array, int nbr_particles, int nbr_iterations, 
 	
 	// print final values of element number 8 of array (array[7])
 	//if(prank == 0) {
-	print_particle(&array[7], prank, psize);
+	//print_particle(&array[7], prank, psize);
 	//}
 }
 
